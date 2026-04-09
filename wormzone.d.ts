d5a99c9c5d71db7f7f39baa489e065a0465afbf1
Aung Koko Lwin <toyko2001@gmail.com>
2026-04-09 15:12:21 +0630

    Deploy WASM build to GitHub Pages

---
+++ wormzone.d.ts
@@ -0,0 +1,53 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly main: (a: number, b: number) => number;
+    readonly wasm_bindgen__convert__closures_____invoke__h55fe377886d7ce87: (a: number, b: number, c: any) => [number, number];
+    readonly wasm_bindgen__convert__closures_____invoke__h29e055d86b500c77: (a: number, b: number, c: any, d: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_3: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_4: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_5: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_6: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_7: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_8: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_9: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h569fdaacb839a94b: (a: number, b: number, c: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__heefdf59f56b38f59: (a: number, b: number) => void;
+    readonly wasm_bindgen__convert__closures_____invoke__h0affa4a7f6e66bb4: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_destroy_closure: (a: number, b: number) => void;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ wormzone_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const wasm_bindgen__convert__closures_____invoke__h55fe377886d7ce87: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h29e055d86b500c77: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h14efffc06d5abf33_9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h569fdaacb839a94b: (a: number, b: number, c: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__heefdf59f56b38f59: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h0affa4a7f6e66bb4: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
